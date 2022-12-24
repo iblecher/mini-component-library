71ce9dfc5b868d3b4f1e9de7c5c8af63a9e0294f
Liron Blecher <iblecher@gmail.com>
2022-12-24 18:58:34 +0100

    implement select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+      <Wrapper>
+        <span>{displayedValue}</span>
+        <IconWrapper id="chevron-down" style={{"--size": "24px"}} />
+        <SelectWrapper value={value} onChange={onChange}>
+          {children}
+        </SelectWrapper>
+      </Wrapper>
   );
 };
+
+const SelectWrapper = styled.select`
+  position: absolute;
+  inset: 0;
+  appearance: none;
+  border: none;
+  opacity: 0;
+`;
+
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  color: ${COLORS.gray700};
+  display: inline-block;
+  margin: auto;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  padding: 12px 48px 12px 16px;
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  border-radius: 8px;
+  border: 2px solid transparent;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+  
+  &:hover ${IconWrapper} {
+    color: ${COLORS.black};
+  }
+  
+  &:focus-within {
+    border-color: ${COLORS.primary};
+  }
+`;
+
+
+
 
 export default Select;
